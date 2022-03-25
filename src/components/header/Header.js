685214e3c2bdd9b74c2571ea1fd2f9aa5262100f
Rashid khan <rashidkhanbd57@gmail.com>
2022-03-25 22:13:41 +0600

    add header and data fetch

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css'
+const Header = () => {
+   return (
+      <div className='container'>
+         <h1 className='header'> The Ultimate Foodie </h1>
+         <hr />
+      </div>
+   );
+};
+
+export default Header;

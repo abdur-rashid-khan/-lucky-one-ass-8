b8533758416e501d55411150bf01f0d4d2a11850
Rashid khan <rashidkhanbd57@gmail.com>
2022-03-26 00:49:47 +0600

    responsive

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,10 +1,22 @@
+import { faShoppingBag, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Cart.css'
 const Cart = () => {
    return (
       <div className='cart'>
          <div className="cart-info">
-            Lorem ipsum dolor sit.
+            <div className="cart-header">
+               <h2>Selected Product </h2>
+            </div>
+            <div className="cart-btn">
+               <button className='Choses-btn'>Choses Product 
+                  <p className='Choses-icon'> <FontAwesomeIcon icon={faShoppingBag}></FontAwesomeIcon> </p>
+               </button>
+            </div>
+            <button className='Clear-btn'>Clear Product 
+               <p className='Choses-icon'> <FontAwesomeIcon icon={faTrash} /> </p>
+            </button>
          </div>
       </div>
    );
